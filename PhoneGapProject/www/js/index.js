20dcaee112d14855a4725848e51f7eb096379268
TopRamen789 <TopRamen789@gmail.com>
2016-03-14 20:30:13 -0600

    Login working
    It almost works!!!!!!!!!!!!!!

--- PhoneGapProject/www/js/index.js
+++ PhoneGapProject/www/js/index.js
@@ -1,18 +1,31 @@
-function signIn()
-{
-
-	if(document.getElementById("userNameInput").value == "" || document.getElementById("passwordInput").value=="")
-	{
- 		alert("Please enter username and password");
- 		document.getElementById("userNameInput").value = "";
- 		document.getElementById("passwordInput").value = "";
+function signIn() {
+	var username = document.querySelector('#userNameInput').value;
+	var password = document.querySelector('#passwordInput').value;
+	var form = document.querySelector('#myForm');
+	if(username == "") {
+ 		form.reset();
+ 		alert("Please enter username.");
+	} else if(password == "") {
+		form.reset();
+		alert("Please enter password.");
 	}
- 	else
- 	{
- 		document.getElementById("userNameInput").value = "";
- 		document.getElementById("passwordInput").value = "";
- 		window.location.assign("tasks.php");
+ 	else {
+ 		$.ajax({
+            url: "http://icarus.cs.weber.edu/~ml18995/SoftwareEngineering/Assignment3/php/GetUserPW.php?username=" + username,
+            success: function (data) {
+            	if(data == "") {
+            		form.reset();
+            		alert("Incorrect username or password.");
+            	} else {
+	            	var d = $.parseJSON(data);
+	                if(sha256(password) == d.password) {
+	                	document.location.href = "tasks.html";
+	                } else {
+	                	form.reset();
+	                	alert("Incorrect password.");
+	                }
+                }
+            }
+        });
 	}
- 
- 
 }
